d5abc6dc2348a929fb1ebf55add2f2cb2d714b1c
김효정 <136438704+KHJ2620@users.noreply.github.com>
2024-07-03 14:23:15 +0900

    Hello_실습

---
+++ Hello_001/src/com/callor/hello/HelloB.java
@@ -0,0 +1,20 @@
+package com.callor.hello;
+
+public class HelloB {
+
+	public static void main(String[] args) {
+		int num1 = 30;
+		int num2 = 40;
+		int sum = num1 + num2;
+		System.out.println(sum);
+		System.out.println(sum);
+		System.out.println(sum);
+		System.out.println(sum);
+		System.out.println(sum);
+		
+		// 변수: 기억장소를 절약하기 위해 사용
+		// 값을 변경할 수 있는 기억장소 
+
+	}
+
+}
---
+++ Hello_001/src/com/callor/hello/HelloD.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+public class HelloD {
+
+	public static void main(String[] args) {
+		
+		int num1 = 100;
+		long num2 = 100;
+		
+
+	}
+
+}
